cb97d0d9704e862647bfaecdf24a40348821d4fd
ohwhytina <74386790+ohwhytina@users.noreply.github.com>
2021-09-13 19:28:28 -0700

    Update index.js

--- client/src/components/BlabMenu/index.js
+++ client/src/components/BlabMenu/index.js
@@ -32,7 +32,7 @@ const BlabMenu = () => {
     }
    
   }, [thisUserData, dispatch]);
-  greeting = "Welecome to the Blabs" + greeting;
+  greeting = "Welcome to the Blabs" + greeting;
   useEffect(() => {
     if (data) {
       dispatch({
